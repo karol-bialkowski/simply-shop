872561d5fb60733ed5446193c962874249486b14
Karol Bialkowski <k.bialkowski10@gmail.com>
2019-12-14 22:14:52 +0100

    - basic http auth to add product

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
