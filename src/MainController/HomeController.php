c8f85c7c01d9e954d8db9a0e79bd689b657dc907
Karol Bialkowski <k.bialkowski10@gmail.com>
2019-12-19 20:29:16 +0100

    - added xdebug option

--- src/MainController/HomeController.php
+++ src/MainController/HomeController.php
@@ -10,11 +10,6 @@ class HomeController extends AbstractController
 
     public function index()
     {
-
-
-        echo 'aaa111';
-        exit;
-
         return $this->render('@main/homepage.html.twig');
     }
 
